24b4b2a1808157b3b84eb9a6dc7c5f736d3d0553
Juman Kaushik <juman.code@gmail.com>
2024-10-20 23:12:12 +0530

    Vans page added using miraz proxy server

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import Vans from './pages/Vans'
+import './server'
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <header>
-          <Link to='/'>#VANLIFE</Link>
+          <Link className="site-logo" to="/">#VanLife</Link>
           <nav>
             <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
           </nav>
         </header>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
+          <Route path='/vans' element={<Vans/>} />
         </Routes>
       </BrowserRouter>
     </>
